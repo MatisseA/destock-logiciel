1891dbc3999bb958d255934b6fa301f1f45c69a8
Matisse <gevyoohd@gmail.com>
2019-04-04 17:02:10 +0200

    Ajout statistique

--- Destock/Destock/Statistique.Designer.cs
+++ Destock/Destock/Statistique.Designer.cs
@@ -31,12 +31,13 @@
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint1 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 30D);
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint2 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 70D);
             System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Statistique));
             this.graphique = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBox_choix = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_générer = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.graphique)).BeginInit();
             this.SuspendLayout();
             // 
@@ -55,21 +56,52 @@
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedBar100;
             series1.Legend = "Legend1";
-            series1.Name = "Annonce premium";
-            series1.Points.Add(dataPoint1);
+            series1.Name = "Series1";
+            series1.YValuesPerPoint = 2;
             series2.ChartArea = "ChartArea1";
             series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedBar100;
             series2.Legend = "Legend1";
-            series2.Name = "Annonce non premium";
-            series2.Points.Add(dataPoint2);
+            series2.Name = "Series2";
             this.graphique.Series.Add(series1);
             this.graphique.Series.Add(series2);
             this.graphique.Size = new System.Drawing.Size(759, 174);
             this.graphique.TabIndex = 0;
             this.graphique.Text = "chart1";
-            title1.Name = "Premium_pr";
-            title1.Text = "Pourcentage d\'annonces premium";
+            title1.Name = "titre_graphique";
+            title1.Text = "Titre";
             this.graphique.Titles.Add(title1);
+            // 
+            // comboBox_choix
+            // 
+            this.comboBox_choix.FormattingEnabled = true;
+            this.comboBox_choix.Items.AddRange(new object[] {
+            "Pourcentage d\'annonces premium",
+            "Nombre total d\'annonce en ligne"});
+            this.comboBox_choix.Location = new System.Drawing.Point(12, 252);
+            this.comboBox_choix.Name = "comboBox_choix";
+            this.comboBox_choix.Size = new System.Drawing.Size(214, 21);
+            this.comboBox_choix.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(66, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Choix du graphique";
+            // 
+            // button_générer
+            // 
+            this.button_générer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_générer.Location = new System.Drawing.Point(695, 445);
+            this.button_générer.Name = "button_générer";
+            this.button_générer.Size = new System.Drawing.Size(75, 23);
+            this.button_générer.TabIndex = 3;
+            this.button_générer.Text = "Générer";
+            this.button_générer.UseVisualStyleBackColor = true;
+            this.button_générer.Click += new System.EventHandler(this.button_générer_Click);
             // 
             // Statistique
             // 
@@ -77,6 +109,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(24)))), ((int)(((byte)(32)))));
             this.ClientSize = new System.Drawing.Size(783, 480);
+            this.Controls.Add(this.button_générer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox_choix);
             this.Controls.Add(this.graphique);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Statistique";
@@ -85,11 +120,15 @@
             this.Load += new System.EventHandler(this.Statistique_Load);
             ((System.ComponentModel.ISupportInitialize)(this.graphique)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart graphique;
+        private System.Windows.Forms.ComboBox comboBox_choix;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_générer;
     }
 }
--- Destock/Destock/Statistique.cs
+++ Destock/Destock/Statistique.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,15 +19,139 @@ namespace Destock
             InitializeComponent();
             this.lien = lien2;
         }
+        public String ChaineConnexion;
+        public MySqlConnection GestConn;
+
+        public String legend1;
+        public String legend2;
+        public String titreGraphique;
 
         private void Statistique_Load(object sender, EventArgs e)
         {
+            connexionBDD();
             statPremiumPourcentage();
+            comboBox_choix.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void reinitialiserValeur()
+        {
+            graphique.Series[legend1].Name = "Series1";
+            graphique.Series[legend2].Name = "Series2";
+
+            legend1 = "Series1";
+            legend2 = "Series2";
+            titreGraphique = "Titre";
+
+            changeEnabledSeries(false, false);
+
+            graphique.Series[legend1].Points.AddXY("", 0);
+            graphique.Series[legend2].Points.AddXY("", 0);
+            graphique.Titles["titre_graphique"].Text = titreGraphique;
         }
 
         private void statPremiumPourcentage()
         {
-            
+            int nbTotal = count("annonce","");
+            int nbPremium = count("annonce", "TYPE_ANNC='premium'");
+
+            legend1 = "Annonce premium";
+            legend2 = "Annonce non premium";
+            titreGraphique = "Pourcentage d'annonces premium";
+
+            //Set les nouveaux noms
+            graphique.Series["Series1"].Name = legend1;
+            graphique.Series["Series2"].Name = legend2;
+
+            changeEnabledSeries(true, true);
+
+            //Change les données du tableau de stats
+            graphique.Series[legend1].Points.AddXY("%", nbPremium);
+            graphique.Series[legend2].Points.AddXY("%", nbTotal-nbPremium);
+
+            //Set le titre
+            graphique.Titles["titre_graphique"].Text = titreGraphique;
+        }
+
+        private void statNbTotalAnnonce()
+        {
+            int nbTotal = count("annonce", "");
+
+            legend1 = "Annonce premium";
+            titreGraphique = "Nombre total d'annonce en ligne";
+
+            //Set les nouveaux noms
+            graphique.Series["Series1"].Name = legend1;
+            changeEnabledSeries(true, false);
+
+            //Change les données du tableau de stats
+            graphique.Series[legend1].Points.AddXY("Annonce", nbTotal);
+
+            //Set le titre
+            graphique.Titles["titre_graphique"].Text = titreGraphique;
+        }
+
+        private void changeEnabledSeries(bool series1, bool series2)
+        {
+            graphique.Series[legend1].Enabled = series1;
+            graphique.Series[legend2].Enabled = series2;
+        }
+
+        private void connexionBDD()
+        {
+            // Chaîne de connexion à la base
+            ChaineConnexion = "server=btsinfo-rousseau53.fr;port=33017;user=2017-destock;password=matissepierre;database=2017-destock_bdd;encrypt=false";
+            //Création de la connexion
+            GestConn = new MySqlConnection(ChaineConnexion);
+        }
+
+        public int count(String from, String where)
+        {
+            int total = 0;
+            String requete = "SELECT * FROM " + from;
+            if(where != "")
+            {
+                requete = requete + " WHERE " + where;
+            }
+            try
+            {
+                //Ouverture connexion
+                GestConn.Open();
+                //Requete SQL
+                MySqlCommand MaCommande = new MySqlCommand(requete, GestConn);
+                //Déclaration de Data Reader
+                MySqlDataReader ReaderCalcul;
+                //Exécution de la requête
+                ReaderCalcul = MaCommande.ExecuteReader();
+                while (ReaderCalcul.Read())
+                {
+                    total++;
+                }
+                ReaderCalcul.Close();
+                GestConn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur : " + ex.Message);
+            }
+            return total;
+        }
+
+        private void button_générer_Click(object sender, EventArgs e)
+        {
+            connexionBDD();
+            reinitialiserValeur();
+            switch (comboBox_choix.Text)
+            {
+                case "Pourcentage d'annonces premium":
+                    statPremiumPourcentage();
+                    break;
+                case "Nombre total d'annonce en ligne":
+
+                    break;
+                default:
+                    MessageBox.Show("Erreur lors du chargement du graphique.");
+                    break;
+            }
         }
     }
 }
