a46d429a4dcc32384e4a2644a68b9692bff02de4
Matisse <gevoohd@gmail.com>
2019-04-05 22:26:05 +0200

    Avancement Statistique

--- Destock/Destock/Statistique.Designer.cs
+++ Destock/Destock/Statistique.Designer.cs
@@ -32,6 +32,7 @@
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Statistique));
             this.graphique = new System.Windows.Forms.DataVisualization.Charting.Chart();
@@ -54,17 +55,21 @@
             this.graphique.Location = new System.Drawing.Point(12, 12);
             this.graphique.Name = "graphique";
             series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedBar100;
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             series1.YValuesPerPoint = 2;
             series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedBar100;
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedArea;
             series2.Legend = "Legend1";
             series2.Name = "Series2";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series3";
             this.graphique.Series.Add(series1);
             this.graphique.Series.Add(series2);
-            this.graphique.Size = new System.Drawing.Size(759, 174);
+            this.graphique.Series.Add(series3);
+            this.graphique.Size = new System.Drawing.Size(759, 414);
             this.graphique.TabIndex = 0;
             this.graphique.Text = "chart1";
             title1.Name = "titre_graphique";
@@ -76,8 +81,11 @@
             this.comboBox_choix.FormattingEnabled = true;
             this.comboBox_choix.Items.AddRange(new object[] {
             "Pourcentage d\'annonces premium",
-            "Nombre total d\'annonce en ligne"});
-            this.comboBox_choix.Location = new System.Drawing.Point(12, 252);
+            "Annonces en lignes/supprimées",
+            "Pourcentage de membre administrateur",
+            "Pourcentage de membre validé",
+            "Argent gagné par l\'entreprise"});
+            this.comboBox_choix.Location = new System.Drawing.Point(475, 445);
             this.comboBox_choix.Name = "comboBox_choix";
             this.comboBox_choix.Size = new System.Drawing.Size(214, 21);
             this.comboBox_choix.TabIndex = 1;
@@ -86,7 +94,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(66, 236);
+            this.label1.Location = new System.Drawing.Point(538, 429);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(98, 13);
             this.label1.TabIndex = 2;
--- Destock/Destock/Statistique.cs
+++ Destock/Destock/Statistique.cs
@@ -24,28 +24,39 @@ namespace Destock
 
         public String legend1;
         public String legend2;
+        public String legend3;
         public String titreGraphique;
+
+        public List<double> listInt;
 
         private void Statistique_Load(object sender, EventArgs e)
         {
             connexionBDD();
-            statPremiumPourcentage();
+            legend1 = "Series1";
+            legend2 = "Series2";
+            legend3 = "Series3";
+            reinitialiserValeur();
             comboBox_choix.DropDownStyle = ComboBoxStyle.DropDownList;
+            listInt = new List<double>();
         }
 
         private void reinitialiserValeur()
         {
             graphique.Series[legend1].Name = "Series1";
             graphique.Series[legend2].Name = "Series2";
+            graphique.Series[legend3].Name = "Series3";
 
             legend1 = "Series1";
             legend2 = "Series2";
+            legend3 = "Series3";
             titreGraphique = "Titre";
 
-            changeEnabledSeries(false, false);
+            changeEnabledSeries(false, false, false);
+            
 
-            graphique.Series[legend1].Points.AddXY("", 0);
-            graphique.Series[legend2].Points.AddXY("", 0);
+            graphique.Series[legend1].Points.Clear();
+            graphique.Series[legend2].Points.Clear();
+            graphique.Series[legend3].Points.Clear();
             graphique.Titles["titre_graphique"].Text = titreGraphique;
         }
 
@@ -59,43 +70,111 @@ namespace Destock
             titreGraphique = "Pourcentage d'annonces premium";
 
             //Set les nouveaux noms
-            graphique.Series["Series1"].Name = legend1;
-            graphique.Series["Series2"].Name = legend2;
-
-            changeEnabledSeries(true, true);
+            setNameSeries();
+            changeChartType("starkedbar100");
+            changeEnabledSeries(true, true, false);
 
             //Change les données du tableau de stats
             graphique.Series[legend1].Points.AddXY("%", nbPremium);
             graphique.Series[legend2].Points.AddXY("%", nbTotal-nbPremium);
-
-            //Set le titre
-            graphique.Titles["titre_graphique"].Text = titreGraphique;
         }
 
+        //Affiche le nombre total d'annonce en ligne
         private void statNbTotalAnnonce()
         {
             int nbTotal = count("annonce", "");
+            int nbAnnonceDeleted = count("annonce", "STATUT_ANNC = 'deleted'");
 
-            legend1 = "Annonce premium";
-            titreGraphique = "Nombre total d'annonce en ligne";
+            legend1 = "Nombre total d'annonce";
+            legend2 = "Annonces en lignes";
+            legend3 = "Annonces supprimées";
+            titreGraphique = "Nombre total d'annonce";
 
             //Set les nouveaux noms
-            graphique.Series["Series1"].Name = legend1;
-            changeEnabledSeries(true, false);
+            setNameSeries();
+
+
+            changeEnabledSeries(true, true, true);
+            changeChartType("column");
 
             //Change les données du tableau de stats
             graphique.Series[legend1].Points.AddXY("Annonce", nbTotal);
-
-            //Set le titre
-            graphique.Titles["titre_graphique"].Text = titreGraphique;
+            graphique.Series[legend2].Points.AddXY("Annonce", nbTotal-nbAnnonceDeleted);
+            graphique.Series[legend3].Points.AddXY("Annonce", nbAnnonceDeleted);
         }
 
-        private void changeEnabledSeries(bool series1, bool series2)
+        private void statMembreAdmin()
+        {
+            int nbAdmin = count("membre", "TYPE_MEMBRE = 'ADMINISTRATEUR'");
+            int nbTotal = count("membre", "TYPE_MEMBRE = 'UTILISATEUR'");
+
+            legend1 = "Membre";
+            titreGraphique = "Poucentage de membre validé";
+
+            //Set les nouveaux noms
+            setNameSeries();
+
+
+            changeEnabledSeries(true, false, false);
+            changeChartType("pie");
+
+            //Change les données du tableau de stats
+            graphique.Series[legend1].Points.AddXY("Utilisateur - ("+nbTotal+")", nbTotal);
+            graphique.Series[legend1].Points.AddXY("Premium - (" + nbAdmin + ")", nbAdmin);
+        }
+
+        private void statMembreValide()
+        {
+            int nbValide = count("membre", "VALIDE_MEMBRE = 1");
+            int nbTotal = count("membre", "VALIDE_MEMBRE = 0");
+
+            legend1 = "Membre";
+            titreGraphique = "Poucentage de membre administrateur";
+
+            //Set les nouveaux noms
+            setNameSeries();
+
+
+            changeEnabledSeries(true, false, false);
+            changeChartType("pie");
+
+            //Change les données du tableau de stats
+            graphique.Series[legend1].Points.AddXY("Utilisateur - (" + nbTotal + ")", nbTotal);
+            graphique.Series[legend1].Points.AddXY("Validé - (" + nbValide + ")", nbValide);
+        }
+
+        private void statArgentDestock()
+        {
+            chargeList("virement", "", "MONTANT_VIR");
+
+            legend1 = "Un virement";
+            legend2 = "Total argent gagné";
+            titreGraphique = "Argent gagné";
+
+            setNameSeries();
+
+            changeEnabledSeries(true, true, false);
+            changeChartType("stackedarea");
+            
+            double totalMontant = 0;
+            //Change les données du tableau de stats
+            foreach (double lemontant in listInt)
+            {
+                totalMontant = totalMontant + lemontant;
+                graphique.Series[legend1].Points.AddXY("", lemontant);
+                graphique.Series[legend2].Points.AddXY("", totalMontant);
+            }
+        }
+
+        //Permet de changer l'etats des Series
+        private void changeEnabledSeries(bool series1, bool series2, bool series3)
         {
             graphique.Series[legend1].Enabled = series1;
             graphique.Series[legend2].Enabled = series2;
+            graphique.Series[legend3].Enabled = series3;
         }
 
+        //Connexion à la base de données
         private void connexionBDD()
         {
             // Chaîne de connexion à la base
@@ -104,6 +183,7 @@ namespace Destock
             GestConn = new MySqlConnection(ChaineConnexion);
         }
 
+        //Compte les éléments, sous certaines conditlions
         public int count(String from, String where)
         {
             int total = 0;
@@ -136,6 +216,84 @@ namespace Destock
             return total;
         }
 
+        public void chargeList(String from, String where, String what)
+        {
+            String requete = "SELECT * FROM " + from;
+            if(where != "")
+            {
+                requete = requete + " WHERE " + where;
+            }
+            requete = requete + " ORDER BY ID_VIR";
+            try
+            {
+                //Ouverture connexion
+                GestConn.Open();
+                //Requete SQL
+                MySqlCommand MaCommande = new MySqlCommand(requete, GestConn);
+                //Déclaration de Data Reader
+                MySqlDataReader ReaderCalcul;
+                //Exécution de la requête
+                ReaderCalcul = MaCommande.ExecuteReader();
+                while (ReaderCalcul.Read())
+                {
+                    listInt.Add(double.Parse(ReaderCalcul[what].ToString()));
+                }
+                ReaderCalcul.Close();
+                GestConn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur : " + ex.Message);
+            }
+        }
+
+        //Permet de mettre le graphique de la façon souhaité
+        public void changeChartType(String disposition)
+        {
+            switch (disposition)
+            {
+                case "column":
+                    graphique.Series[legend1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+                    graphique.Series[legend2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+                    graphique.Series[legend3].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+                    break;
+                case "starkedbar100":
+                    graphique.Series[legend1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedBar100;
+                    graphique.Series[legend2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedBar100;
+                    graphique.Series[legend3].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedBar100;
+                    break;
+                case "pie":
+                    graphique.Series[legend1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+                    graphique.Series[legend2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+                    graphique.Series[legend3].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+                    break;
+                case "line":
+                    graphique.Series[legend1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                    graphique.Series[legend2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                    graphique.Series[legend3].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                    break;
+                case "stackedarea":
+                    graphique.Series[legend1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedArea;
+                    graphique.Series[legend2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedArea;
+                    graphique.Series[legend3].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedArea;
+                    break;
+                default:
+                    graphique.Series[legend1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+                    graphique.Series[legend2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+                    graphique.Series[legend3].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+                    break;
+            }
+        }
+
+        //Actualise les nom Series
+        private void setNameSeries()
+        {
+            graphique.Series["Series1"].Name = legend1;
+            graphique.Series["Series2"].Name = legend2;
+            graphique.Series["Series3"].Name = legend3;
+            graphique.Titles["titre_graphique"].Text = titreGraphique;
+        }
+
         private void button_générer_Click(object sender, EventArgs e)
         {
             connexionBDD();
@@ -145,8 +303,17 @@ namespace Destock
                 case "Pourcentage d'annonces premium":
                     statPremiumPourcentage();
                     break;
-                case "Nombre total d'annonce en ligne":
-
+                case "Annonces en lignes/supprimées":
+                    statNbTotalAnnonce();
+                    break;
+                case "Pourcentage de membre administrateur":
+                    statMembreAdmin();
+                    break;
+                case "Pourcentage de membre validé":
+                    statMembreValide();
+                    break;
+                case "Argent gagné par l'entreprise":
+                    statArgentDestock();
                     break;
                 default:
                     MessageBox.Show("Erreur lors du chargement du graphique.");
